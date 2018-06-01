ef51db81c92cf6018b791831161fa566f00a2e13
Diego Campanari <campanari@msn.com>
2018-06-01 12:16:19 -0300

    Adicionado exposiÃÃo dos mÃdulos angular e bootstrap

--- Lab4/server.js
+++ Lab4/server.js
@@ -2,10 +2,11 @@ var express = require('express');
 
 var app = express();
 
-//app.use(express.static('node_modules'));
+app.use('/angular', express.static('node_modules/angular'));
+app.use('/bootstrap', express.static('node_modules/bootstrap'));
 
 app.use(express.static(__dirname + '/'));
 
 app.listen(2319, function () {
     console.log('Our app is listing on port 2319!');
-});ß
+});
