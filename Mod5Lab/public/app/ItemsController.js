dc507dec24c4da3fea6cb85bcc2c33a420f8eccd
Diego Campanari <campanari@msn.com>
2018-06-01 21:30:16 -0300

    Início do módulo 5

---
+++ Mod5Lab/public/app/ItemsController.js
@@ -0,0 +1,8 @@
+angular
+    .module('app')
+    .controller('ItemsController', [
+        function () {
+            var vm = this;
+            vm.items = ['First', 'Second', 'Third'];
+        }
+    ]);
