9e5d74ee70b067d75803d040261d05ec88a6533e
Diego Campanari <campanari@msn.com>
2018-06-04 19:31:46 -0300

    Testes unitários finalizados

--- Mod6Lab/server.js
+++ Mod6Lab/server.js
@@ -1,7 +1,11 @@
 'use strict';
-var express = require('express'),
-    app = express();
-var http = require('http').Server(app);
+var express = require('express');
+
+var app = express();
+
+var http = require('http')
+    .Server(app);
+
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
@@ -11,11 +15,13 @@ app.use(function (req, res, next) {
 });
 
 console.log('dirname', __dirname);
+
 app.use(express.static(__dirname));
-var server = app.listen('3000', function() {
+
+var server = app.listen('3000', function () {
     var host = server.address().address;
     var port = server.address().port;
-    console.log('listening at port:'+ port);
+    console.log('listening at port:' + port);
 });
 
 module.exports = app;
